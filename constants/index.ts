01c60ed1a6f3b6159d9bd02ef566bf342fc16c1a
demhat <mail.demhatsahin@gmail.com>
2024-09-19 14:34:39 +0300

    Chore: Update some details

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Cusp Nuxt 3'
-export const appDescription = 'Cusp Nuxt 3'
+export const appName = 'Cusp Nuxt 3 by Shaoula'
+export const appDescription = 'Cusp Nuxt 3 by Shaoula'
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <div />
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      Cusp Nuxt 3 by Shaoula
+    </p>
+  </div>
 </template>
